6373f1afe21faefc027bb3bf2eca45411eb79d97
alexbirukov <lord_maloy@mail.ru>
2013-01-21 00:01:19 +0600

    Add Russian language
    Add Russian language file

---
+++ lang/lang.ru.php
@@ -0,0 +1,24 @@
+<?php
+
+	$about = array(
+		'name' => 'Русский',
+		'author' => array(
+			'name' => 'Александр Бирюков',
+			'email' => 'info@alexbirukov.ru',
+			'website' => 'http://alexbirukov.ru'
+		),
+		'release-date' => '2013-01-20'
+	);
+
+	/**
+	 * Number Field
+	 */
+	$dictionary = array(
+
+		'Number' => 
+		'Число',
+
+		'Must be a number.' => 
+		'Укажите корректное число.',
+
+	);
